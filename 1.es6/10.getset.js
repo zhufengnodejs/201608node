52b263305d9c769be14d110ac9d4231476f8446d
zhufengnodejs <zhufengnodejs@126.com>
2016-10-20 16:11:27 +0800

    tmpl

--- 1.es6/10.getset.js
+++ 1.es6/10.getset.js
@@ -9,12 +9,12 @@ class Document{
     }
 
     set cookie(cookie){
-        var key = cookie.split('=')[0];
-        /**
+       /* var key = cookie.split('=')[0];
+        /!**
          * 1. map进行映射成新数组
          * 2. filter先过滤，再追加
          * 3. key的提取可以用正则也可以用split分割
-         */
+         *!/
         this.cookies = this.cookies.filter(function(item){
               //如果传入的key和当前迭代到的item的key相同，则过滤掉，否则留下
                if(key == item.split('=')[0]){
@@ -22,12 +22,26 @@ class Document{
                }
                return true;
         });
-        this.cookies.push(cookie);
+        this.cookies.push(cookie);*/
         /**
          * 1. 先获取一个跟传入的key相同的原来原素的的索引
          * 2. 如果等于-1，表示没有找到，则直接push
          * 3. 如果找到，则删除原来的元素后追加
          */
+        var newKey=cookie.split('=')[0];
+        //var newKey = cookie.match(/(\w+)=\w+/)[1];
+        var index=this.cookies.findIndex(function(item){
+            if(newKey==item.split('=')[0]){
+                return true;
+            }else{
+                return false;
+            }
+        });
+        if(index !==-1){
+            this.cookies.splice(index,1);
+        }
+        this.cookies.push(cookie)
+
     }
 }
 var document = new Document();
