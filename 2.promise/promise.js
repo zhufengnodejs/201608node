5666d7e974b92740a0338c846d047c97a7f19b3a
zhufengnodejs <zhufengnodejs@126.com>
2016-10-20 17:30:32 +0800

    tmpl

--- 2.promise/promise.js
+++ 2.promise/promise.js
@@ -5,3 +5,16 @@
  * 4. 如果调用 resolve调用成功回调
  * 5. 如果调用reject调用失败的回调
  */
+function Promise(fn){
+   //1.声明一个resolve
+   var resolve = data =>  this.onSuccess(data);
+   //2. 声明一个reject
+   var reject = error => this.onFail(error);
+   fn(resolve,reject);
+}
+Promise.prototype.then = function(onSuccess,onFail){
+    this.onSuccess = onSuccess;
+    this.onFail= onFail;
+}
+
+module.exports = Promise;
