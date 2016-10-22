9c18f3e796fbff7c97246c82237ee2e1f2795d2b
zhufengnodejs <zhufengnodejs@126.com>
2016-10-22 16:26:50 +0800

    个人设置

--- 3.express/8.tmpl.js
+++ 3.express/8.tmpl.js
@@ -4,6 +4,7 @@
  */
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
 var app = express();
 //设置模板引擎为EJS，模板要按EJS规定的格式来写，渲染的时候也要按照ejs来进行渲染
 app.set('view engine','ejs');
@@ -24,8 +25,13 @@ app.get('/public/bootstrap.min.css',function(req,res){
  */
 
 app.get('/users',function(req,res){
-    //模板是一个相对路径，
-   res.render('users',{title:'用户列表'});
+    //读取user.json文件内容，得到一个JSON字符串
+    fs.readFile('users.json','utf8',function(err,data){
+        //把此字符串转成JSOn对象数组
+        var users  = JSON.parse(data);
+        //渲染模板 users.ejs是模板的相对路径
+        res.render('users',{title:'用户列表',users:users});
+    })
 });
 // /public/bootstrap.min.css
 app.listen(8080);
