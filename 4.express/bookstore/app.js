76ca26d94ac4d99144f887f96c6cc926315637cb
zhufengnodejs <zhufengnodejs@126.com>
2016-10-23 15:32:16 +0800

    编写路由

--- 4.express/bookstore/app.js
+++ 4.express/bookstore/app.js
@@ -1,3 +1,13 @@
+var express = require('express');
+var app = express();
 /**
- * zhangrenyang
+ * 1. 引入静态文件中间件
+ * 2. 配置模板 模板引擎+模板的存放目录+html后缀渲染方式
+ * 3. 引入bodyParser中间件 req.body属性
  */
+
+app.get('/',function(req,res){
+  res.render('index',{title:'首页'});
+});
+//........
+app.listen(8080);
