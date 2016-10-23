b74e5d53313cfb6d990f08a20d47288dc2344032
zhufengnodejs <zhufengnodejs@126.com>
2016-10-23 13:11:56 +0800

    模板的基本原理

--- t.js
+++ t.js
@@ -1,22 +1,2 @@
-/*
-var express = require('express');
-var app = express();
-app.set('name','zfpx');
-app.name = 'zfpx';
-console.log(app.get('name'));
-console.log(app.name);
-var path = require('path');
-console.log(path.join('a','b'));*/
-var name = 'zfpx';
-var age = 8;
-var tmpl = '${name} is ${age} years old';
-function render(tmpl){
-    return tmpl.replace(/\$\{(\w+)\}/g, function (t1,t2) {
-        return eval(t2);
-    })
-}
-/*var attr = 'name';
-console.log(eval(attr));
-return 'zfpx is 8 years old';*/
-console.log(render(tmpl));
-//'zfpx is 8 years old'
+var querystring = require('querystring');
+console.log(querystring.parse('name=zfpx&age=9'));
