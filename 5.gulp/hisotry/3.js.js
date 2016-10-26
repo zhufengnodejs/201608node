8eadb6f54891b92233ca336744eeb468409c9df0
zhufengnodejs <zhufengnodejs@126.com>
2016-10-26 12:23:40 +0800

    4.css

--- 5.gulp/hisotry/3.js.js
+++ 5.gulp/hisotry/3.js.js
@@ -10,24 +10,23 @@
  3. 处理完成后把处理后的结果导入到gulp.dest可写流中进行保存到文件系统中
  **/
 var gulp = require('gulp');
-var less = require('gulp-less');
 var concat = require('gulp-concat');
-var minifyCss = require('gulp-minify-css')
+var uglify = require('gulp-uglify')
 var rename = require('gulp-rename')
 /**
  * 文件流里里是一个一个文件对象
  * 文件对象属性 1. 文件名 2 文件内容
  */
-gulp.task('css',function(){
-   gulp.src('app/less/*.less')
-   .pipe(less())
-   .pipe(concat('all.css'))
-   .pipe(gulp.dest('./dist/css'))
-   .pipe(minifyCss())
-   .pipe(rename(function (path) {
-      path.basename += ".min";// all   all.min
-   }))
-   .pipe(gulp.dest('./dist/css'))
+gulp.task('js',function(){
+     //指定要合并的文件，如果有多个文件夹的话可以放置一个数组
+   gulp.src(['app/js/bb.js','app/js/aa.js'])
+     //把多个JS文件合并成一个文件并指定合并后的文件名
+    .pipe(concat('all.js'))
+    .pipe(gulp.dest('./dist/js'))
+    .pipe(uglify())
+    .pipe(rename('all.min.js'))
+     //把合并后的唯一的文件all.js保存到dist/js目录下
+    .pipe(gulp.dest('./dist/js'))
 });
 //fs.createReadStream().pipe(fs.createWriteStream());
 
