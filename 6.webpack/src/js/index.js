2c6f5f427d04aeb00576b1959e29981218f4dee0
zhufengnodejs <zhufengnodejs@126.com>
2016-10-26 15:33:32 +0800

    4.css

---
+++ 6.webpack/src/js/index.js
@@ -0,0 +1,2 @@
+var hello = require('./component');
+alert(hello);
---
+++ 6.webpack/webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    //入口文件
+    entry:'./src/js/index.js',
+    output:{//指定输出的位置
+        path:'./build',//指定输出的文件夹
+        filename:'bundle.js'//指定输出的文件名
+    }
+}
