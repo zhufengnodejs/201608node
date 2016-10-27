dcea9a4166a7bf8c5d345541d93bf3c61cfda189
zhufengnodejs <zhufengnodejs@126.com>
2016-10-27 17:31:35 +0800

    加载bootstrap

---
+++ 7.react/js/11.nav.js
@@ -0,0 +1,31 @@
+//演示如何操作兄弟组件 refs
+var Nav = React.createClass({
+    handleClick(num){
+      //console.log(this.refs);
+      for(var attr in this.refs){
+          this.refs[attr].refs[attr].className = '';
+      }
+      this.refs[num].refs[num].className = 'active';
+    },
+    render(){
+        return (
+            <nav>
+                <ul className="pagination">
+                    <Li ref="1" click={this.handleClick} num="1"></Li>
+                    <Li ref="2" click={this.handleClick} num="2"></Li>
+                    <Li ref="3" click={this.handleClick} num="3"></Li>
+                </ul>
+            </nav>
+        )}
+});
+var Li = React.createClass({
+    handlClick(){
+        this.props.click(this.props.num);
+    },
+    render(){
+        return (
+             <li ref={this.props.num} onClick={this.handlClick}><a href="#">{this.props.num}</a></li>
+        )
+    }
+});
+ReactDOM.render(<Nav/>,document.getElementById('app'));
