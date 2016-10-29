14ae95e1aac05daaf5229a21ede3b6ab7f24a315
zhufengnodejs <zhufengnodejs@126.com>
2016-10-29 16:24:56 +0800

    cookie的原理

--- 9.session/app.js
+++ 9.session/app.js
@@ -7,19 +7,32 @@
  */
 var express = require('express');
 var uuid = require('uuid');
+var cookieParser = require('cookie-parser');
 var app = express();
+app.use(cookieParser());
 const CARD_NO = 'connect.sid';
 //这是服务器的一个保存session数据的对象
 var SESSIONS = {};
 // /visit?name=zfpx
+// http://localhost:9090/visit?name=%E9%A9%AC%E8%93%89
 app.get('/visit',function(req,res){
-  var name = req.query.name;
-  //生成一个全世界唯一的卡号
-  var cardNo =uuid.v4();
-  //在服务器端开辟内存，记录卡号和数据对象的对应关系
-  SESSIONS[cardNo] = {name,balance:100};
-  //通过cookie把此卡号发给客户端 ，客户端保存到本地
-  res.cookie(CARD_NO,cardNo);
-  res.send('欢迎你新顾客，送你一张价格100元的理发卡');
+  var name = req.query.name;//获取用户名
+  //从客户端请求对象中的cookie中获取客户卡号
+  var cardNo = req.cookies[CARD_NO];
+  if(cardNo){
+    var cardObj = SESSIONS[cardNo];
+    if(cardObj){
+      cardObj.balance-=10;
+      res.send('欢迎'+name+'，你的理发卡还剩'+cardObj.balance+'元');
+    }
+  }else{
+    //生成一个全世界唯一的卡号
+    var cardNo =uuid.v4();
+    //在服务器端开辟内存，记录卡号和数据对象的对应关系
+    SESSIONS[cardNo] = {name,balance:100};
+    //通过cookie把此卡号发给客户端 ，客户端保存到本地
+    res.cookie(CARD_NO,cardNo);
+    res.send('欢迎'+name+'，送你一张价格100元的理发卡');
+  }
 });
 app.listen(9090);
---
+++ 9.session/test.js
@@ -0,0 +1,4 @@
+var str = '马容';
+// %E9%A9%AC  %E5%AE%B9
+// <Buffer e9 a9 ac e5 ae b9>
+console.log(new Buffer(str));
