299d165cd5c49866caf47c2230562a59abf4b9ae
zhufengnodejs <zhufengnodejs@126.com>
2016-10-29 11:21:51 +0800

    cookie的原理

--- 8.cookie/2.express.js
+++ 8.cookie/2.express.js
@@ -6,8 +6,23 @@ var app = express();
 //使用此中间件后会在req.cookies=cookieObj
 app.use(cookieParser());
 //如何写cookie  res.cookie(name,value); res.cookie('count',1);
+// res.setHeader('Set-Cookie','count=1');
+/**
+ * 1. 获取请求对象中的cookie  req.cookies
+ * 2. 如何向客户端写入cookie res.cookie()
+ */
 app.get('/visit',function(req,res){
-
+    //得到cookie对象
+   var cookie = req.cookies;
+    //判断此对象如果存在并且count有值的话
+    if(cookie && cookie.count){
+        cookie.count++;
+        res.cookie('count',cookie.count);
+        res.send('欢迎你老顾客的第'+cookie.count+'次光临')
+    }else {
+        res.cookie('count',1);
+        res.send('欢迎你新顾客的第1次光临')
+    }
 });
 
 
