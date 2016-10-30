db42adc6a7d39478f4cf8227ea11055598cb1eb3
zhufengnodejs <zhufengnodejs@126.com>
2016-10-30 15:17:12 +0800

    cookie的原理

--- 10.mongoose/User.js
+++ 10.mongoose/User.js
@@ -1,20 +1,29 @@
 var User = {
     data: [{age: 1}, {age: 2}, {age: 3}, {age: 4}, {age: 5}, {age: 6}, {age: 7}, {age: 8}, {age: 9}, {age: 10}],
     skip(num){
-
+       this._skip = num;
+       return this;
     },
     limit(num){
-
+        this._limit = num;
+        return this;
     },
     sort(sortObj){
-
+        var attr = Object.keys(sortObj)[0];
+        this._sort = function(a,b){
+            return (a[attr]-b[attr])*sortObj[attr];
+        };
+        return this;
     },
     exec(callback){
-
+       process.nextTick(()=>{
+           callback(this.data.sort(this._sort).slice(this._skip,this._skip+this._limit));
+       });
+        return this;
     }
 }
 var pageNum = 2;
 var pageSize = 3;
-User.skip(pageSize * (pageNum - 1)).limit(pageSize).sort({age: 1}).exec(function (docs) {
+User.exec(function (docs) {
     console.log(docs);// 4 5 6
-});
+}).skip(pageSize * (pageNum - 1)).limit(pageSize).sort({age: -1})
