5426b4d5db6037e6c90160ec3d040ed0ba7049de
zhufengnodejs <zhufengnodejs@126.com>
2016-11-02 12:44:53 +0800

    2

--- 7.react/js/16.ajax.js
+++ 7.react/js/16.ajax.js
@@ -54,3 +54,43 @@ var Suggest = React.createClass({
     }
 });
 ReactDOM.render(<Suggest></Suggest>,document.getElementById('app'));
+/*var options =
+   {url:'https://www.baidu.com/su',
+    method:'GET',
+    data:{wd:word},//转成查询字符串并拼在URL的后面
+    dataType:'jsonp', //指定响应的类型
+    jsonp:'cb',//指定向服务器端传入的回调函数名的参数名
+    context:this,//指定 success中的this对象 指向当前组件的实例
+    success:function(){}
+}*/
+function ajax({url,method='GET',data,dataType,jsonp,context,success}){
+   // ?cb=jQuery_1478056916822&wd=b&_=1478056916824
+   var callbackName = `jQuery_${Date.now()}`;
+
+   url = addSearch(url,`${jsonp}=${callbackName}`);
+    //-> https://www.baidu.com/su?cb=jQuery_1478056916822
+   url = addSearch(url,toStr(data));
+   url = addSearch(url,`_=${Date.now()}`);
+   var script = document.createElement('script');
+   script.src = url;
+   document.body.appendChild(script);
+   window[callbackName] = function(data){
+       success.call(context,data);
+       delete window[callbackName];
+       script.parentNode.removeChild(script);
+   }
+
+
+}
+//{name:'zfpx',age:9} -> name=zfpx&age=9
+function toStr(data){
+    var parts = [];
+    for(var attr in data){
+        parts.push(`${attr}=${data[attr]}`);
+    }
+    return parts.join('&');
+}
+function addSearch(url,query){
+    return url + (/\?/.test(url)?'&':'?') +query;
+}
+//ajax(options);
