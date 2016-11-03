22e28f8aad775209963720954d6b0d6918d86e55
zhufengnodejs <zhufengnodejs@126.com>
2016-11-03 18:15:32 +0800

    cookie的原理

--- 6.webpack/webpack.message.js
+++ 6.webpack/webpack.message.js
@@ -1,4 +1,6 @@
+//自动产出html文件，在html文件可以自动插入生成的脚本
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+//当webpack打包完成后自动打开浏览器窗口并访问指定的url
 var OpenBrowserWebpackPlugin = require('open-browser-webpack-plugin');
 // npm install html-webpack-plugin open-browser-webpack-plugin --save
 module.exports = {
@@ -8,9 +10,9 @@ module.exports = {
         filename:'bundle.js'
     },
     //指定静态文件根目录
-    devServer:{
-      inline:true,
-      contentBase:'./build'
+    devServer:{//在本地服务器上启动一个服务，监听 8080端口。并以build目录作为静态文件根目录
+      inline:true,//当源代码改变后自动重新打包，然后刷新浏览器
+      contentBase:'./build'//静态文件根目录
     },
     module:{
         loaders:[
@@ -28,10 +30,12 @@ module.exports = {
         ]
     },
     plugins:[
+        //通过此插件可以根据模板自动HTML文件，并且自动插入产出后的文件
         new HtmlWebpackPlugin({
             title: '珠峰留言版',
             template: './message/index.html'
         }),
+        //自动打开浏览器窗口
         new OpenBrowserWebpackPlugin({url:'http://localhost:8080'})
     ]
 }
