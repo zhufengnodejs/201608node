29189fbf73b6dc0e0be498be24d4a4e3a2c020ed
zhufengnodejs <zhufengnodejs@126.com>
2016-11-03 17:41:13 +0800

    cookie的原理

--- 6.webpack/message/components/Message.js
+++ 6.webpack/message/components/Message.js
@@ -3,7 +3,7 @@ export default class Message extends React.Component {
     render() {
         return (
             <li className="list-group-item">{this.props.author}:{this.props.children}
-                <button onClick={this.props.click.bind(this,this.props.id)} className="btn btn-danger">删除</button> <span className="pull-right">{this.props.date&&this.props.date.toLocaleString()}</span>
+                <button onClick={()=>this.props.click(this.props.id)} className="btn btn-danger">删除</button> <span className="pull-right">{this.props.date&&this.props.date.toLocaleString()}</span>
             </li>
         )
     }
