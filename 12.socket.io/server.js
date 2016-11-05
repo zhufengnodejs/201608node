1e6fe95458cee75fb85c372869d1b00bd640c9e3
zhufengnodejs <zhufengnodejs@126.com>
2016-11-05 12:17:43 +0800

    2

---
+++ 12.socket.io/server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var log = console.log;
+var app = express();
+app.get('/',function(req,res){
+    //resolve是得到一个文件的绝对路径
+    res.sendFile(path.resolve('index.html'));
+});
+var server = require('http').createServer(app);
+//io = websocket 服务器端实例
+var io = require('socket.io')(server);
+//监听客户端的请求，请求到达服务器的时候执行回调函数，并传递为此连接创建socket对象
+io.on('connection',function(socket){
+   socket.send('欢迎你客户端');
+   //socket.emit('message','欢迎你客户端');
+   //监听客户端发过来的消息
+   socket.on('message',function(msg){
+     log(msg);
+   });
+});
+server.listen(8080);
