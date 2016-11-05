1256460d94daf940832c43f8085db9776a6e69c6
zhufengnodejs <zhufengnodejs@126.com>
2016-11-05 18:03:46 +0800

    socket.io

--- 12.socket.io/server.js
+++ 12.socket.io/server.js
@@ -24,10 +24,16 @@ var sockets = {};
 //监听客户端的请求，请求到达服务器的时候执行回调函数，并传递为此连接创建socket对象
 io.on('connection',function(socket){
     var username;//此客户端的用户名
+    var currentRoom;
     //每当客户端连接上来的时候，向客户端发送一个消息
     socket.emit('messages',messages);
     socket.send({username:'系统',content:'请输入呢称!',createAt:new Date()});
    //socket.emit('message','欢迎你客户端');
+    //在服务器接收到join事件之后，服务器端对应socket进入某个房间内
+    socket.on('join',function(room){
+        currentRoom = room;
+        socket.join(room);
+    });
    //监听客户端发过来的消息
    socket.on('message',function(msg){
        //如果用户已经设置过了，表示正常的发言
@@ -48,7 +54,12 @@ io.on('connection',function(socket){
            }else{
                messages.push({username,content:msg,createAt:new Date()});
                //向连接到此服务器上的所有的客户端发送消息
-               io.emit('message',{username,content:msg,createAt:new Date()});
+                if(currentRoom){
+                    io.in(currentRoom).emit('message',{username,content:msg,createAt:new Date()});
+                }else{
+                    io.emit('message',{username,content:msg,createAt:new Date()});
+                }
+
            }
        }else{
            //判断此用户是否已经有人用过了，用过的话会提示呢称已经被占用，请重新输入
@@ -72,4 +83,14 @@ server.listen(8080);
  3. 把此用户名和此socket关联起来，把此用户名存放在函数内的私有作用域
  4. 以后此socket再发言，就表示此用户名发言了。
  5. 以后前后端发数据的时候应该发对象了{username:'张三',content:'内容',createAt:2016}
+ **/
+
+/**
+ * 物以类聚
+ * 根据锚点 # 来区分不同的房间
+ * /
+ * /#red 红房间
+ * /#green 绿房间
+ * /#pink 进入粉红房间
+ * 在房间内的发言听不到，在房间的广播房间外和大厅都听不到
  **/
