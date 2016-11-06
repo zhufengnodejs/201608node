6c84b4e1472367c438621967c1dd070e7ff80b4e
zhufengnodejs <zhufengnodejs@126.com>
2016-11-06 10:43:18 +0800

    socket.io

---
+++ 13.crawl/1.request.js
@@ -0,0 +1,17 @@
+// http://top.baidu.com/category?c=1&fr=topindex
+//一个http请求工具，用来向HTTP服务器发起请求
+var request = require('request');
+//用于把gbk的 buffer转成字符串
+var iconv = require('iconv-lite');
+/**
+ * 1.告诉 request不要帮我们把buffer转字符串 ，我们要自己转
+ * 2.gbk的 buffer转成字符串
+ */
+request({url:'http://top.baidu.com/category?c=1&fr=topindex',encoding:null},function(err,response,body){
+    if(!err && response.statusCode == 200){
+        //1参数是要转字符串的buffer,第二个参数是此buffer的编写
+        // utf8
+        body = iconv.decode(body,'gbk');
+        console.log(body);
+    }
+});
